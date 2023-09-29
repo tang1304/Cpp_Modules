3eb51dee27486c75407bba96d750348c8ebe6421
tang1304 <tanguy.gellon@gmail.com>
2023-09-29 14:20:01 +0200

    debut magaphone

---
+++ Cpp00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/29 13:53:47 by tgellon           #+#    #+#             */
+/*   Updated: 2023/09/29 14:19:16 by tgellon          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+	else{
+		for (int i = 1, i < argc, i++){
+			cout
+		}
+	}
+}
