4e7a47eedb38e085a9a3568814165cdf4617bf98
tgellon <tanguy.gellon@gmail.com>
2023-10-09 19:42:03 +0200

    ex 00 seems OK

--- Cpp00/ex00/megaphone.cpp
+++ Cpp00/ex00/megaphone.cpp
@@ -10,15 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <iostream>
+#include <iostream>
+#include <string>
 
 int main(int argc, char **argv)
 {
+	std::string str;
 	if (argc == 1)
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else{
-		for (int i = 1, i < argc, i++){
-			cout
+		for (int i = 1; i < argc; i++){
+			str = argv[i];
+			for (size_t j = 0; j < str.length(); j++)
+			std::cout << static_cast<char>(toupper(str[j]));
 		}
+		std::cout << std::endl;
 	}
+	return (0);
 }
