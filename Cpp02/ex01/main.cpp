b59ed22c5cd53e745c586ecc88b1c28467eb7119
tang1304 <tanguy.gellon@gmail.com>
2023-11-06 15:46:09 +0100

    Fixed ctrl-d on phonebook, was only on the command prompt

--- Cpp02/ex01/main.cpp
+++ Cpp02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 08:43:59 by tgellon           #+#    #+#             */
-/*   Updated: 2023/11/06 11:22:15 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/11/06 13:23:45 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,10 @@ int main(){
 	std::cout << BLUE << "a nbr is : " << a.getRawBits() << DEFAULT_COLOR << std::endl;
 	std::cout << GREEN << "b nbr is : " << b.getRawBits() << DEFAULT_COLOR << std::endl;
 	std::cout << YELLOW << "c nbr is : " << c.getRawBits() << DEFAULT_COLOR << std::endl;
+	std::cout << RED << "d nbr is : " << d.getRawBits() << DEFAULT_COLOR << std::endl;
+
+	std::cout << BLUE << "a nbr is : " << a.toInt() << " as integer" DEFAULT_COLOR << std::endl;
+	std::cout << GREEN << "b nbr is : " << b.toInt() << " as integer" << DEFAULT_COLOR << std::endl;
+	std::cout << YELLOW << "c nbr is : " << c.toInt() << " as integer" << DEFAULT_COLOR << std::endl;
+	std::cout << RED << "d nbr is : " << d.toInt() << " as integer" << DEFAULT_COLOR << std::endl;
 }
