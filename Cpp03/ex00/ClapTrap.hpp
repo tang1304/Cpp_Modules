88d634944e06d0e7196e3757c61e6fa01368da80
Tanguy Gellon <tgellon@z4r8p1.42lyon.fr>
2023-11-08 16:25:05 +0100

    cpp03 ex00 started

--- Cpp03/ex00/ClapTrap.hpp
+++ Cpp03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 13:21:57 by tgellon           #+#    #+#             */
-/*   Updated: 2023/11/08 13:34:10 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/11/08 16:00:54 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,34 @@
 
 # include <iostream>
 
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define PINK "\033[35m"
+# define DEFAULT_COLOR "\033[0m"
+
 class ClapTrap{
 	
 private:
 
 	std::string	_name;
-	int			_hitPoints = 10;
-	int			_energyPoints = 10;
-	int			_AttackDamage = 0;
+	int			_hitPoints;
+	int			_energyPoints;
+	int			_attackDamage;
 
 public:
 
 	ClapTrap();
+	ClapTrap(std::string name);
 	ClapTrap(const ClapTrap &cpy);
 	~ClapTrap();
+	ClapTrap &operator=(const ClapTrap &cpy);
 
 	void	attack(const std::string &target);
 	void	takeDamage(unsigned int amount);
 	void	beRepaired(unsigned int amount);
 
-	ClapTrap &operator=(const ClapTrap &cpy);
 };
 
 #endif
