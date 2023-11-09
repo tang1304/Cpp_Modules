3ab2d8ce823c91ae5ac5a54e8a2eda0a75ee4c00
tang1304 <tanguy.gellon@gmail.com>
2023-11-09 14:37:15 +0100

    cpp03 ex00 ok, starting ex01

--- Cpp02/ex01/main.cpp
+++ Cpp02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 08:43:59 by tgellon           #+#    #+#             */
-/*   Updated: 2023/11/07 15:42:15 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/11/09 12:55:33 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,12 @@ int main(){
 	Fixed	const d(b);
 
 	a = Fixed (1234.4321f);
-	
+
 	std::cout << BLUE << "a nbr is : " << a << DEFAULT_COLOR << std::endl;
 	std::cout << GREEN << "b nbr is : " << b << DEFAULT_COLOR << std::endl;
+	std::cout << GREEN << "b raw is : " << b.getRawBits() << DEFAULT_COLOR << std::endl;
 	std::cout << YELLOW << "c nbr is : " << c << DEFAULT_COLOR << std::endl;
+	std::cout << YELLOW << "c raw is : " << c.getRawBits() << DEFAULT_COLOR << std::endl;
 	std::cout << RED << "d nbr is : " << d << DEFAULT_COLOR << std::endl;
 
 	std::cout << BLUE << "a nbr is : " << a.toInt() << " as integer" DEFAULT_COLOR << std::endl;
---
+++ Cpp03/ex01/ScavTrap.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 14:34:49 by tgellon           #+#    #+#             */
+/*   Updated: 2023/11/09 14:35:35 by tgellon          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+class ScavTrap
+{
+private:
+	/* data */
+public:
+	ScavTrap(/* args */);
+	~ScavTrap();
+};
+
+ScavTrap::ScavTrap(/* args */)
+{
+}
+
+ScavTrap::~ScavTrap()
+{
+}
+
+
+#endif
