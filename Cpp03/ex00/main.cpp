5f979ef8324f1a0c4fb4341b37129a4461393b5a
tang1304 <tanguy.gellon@gmail.com>
2023-11-09 10:39:56 +0100

    working on cpp03 ex00

--- Cpp03/ex00/main.cpp
+++ Cpp03/ex00/main.cpp
@@ -6,12 +6,21 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 13:20:49 by tgellon           #+#    #+#             */
-/*   Updated: 2023/11/08 13:21:10 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/11/09 10:24:39 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 int main (){
-	;
+	std::string	nameA = "CL4P-TP";
+	std::string	nameB = "R2D2";
+
+	ClapTrap	alpha("nameA");
+	ClapTrap	beta("nameB");
+	ClapTrap	gamma;
+
+	gamma = alpha;
+
+	
 }
