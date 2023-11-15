155d2c2d42861acea5368ae9bcc2e0764737773b
tang1304 <tanguy.gellon@gmail.com>
2023-11-15 15:38:01 +0100

    cpp04 ex03 Materias code ok

--- Cpp04/ex03/srcs/AMateria.cpp
+++ Cpp04/ex03/srcs/AMateria.cpp
@@ -6,16 +6,34 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 09:09:04 by tgellon           #+#    #+#             */
-/*   Updated: 2023/11/15 09:19:08 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/11/15 14:02:24 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/AMateria.hpp"
 
-AMateria::AMateria(){
-	;
+AMateria::AMateria(): _type("none"){
 }
 
-AMateria::AMateria(std::string const & type){
+AMateria::AMateria(std::string const & type): _type(type){
+}
+
+AMateria::AMateria(const AMateria &old){
+	*this = old;
+}
+
+AMateria::~AMateria(){
+}
+
+AMateria	&AMateria::operator=(const AMateria &old){
+	this->_type = old._type;
+	return (*this);
+}
+
+std::string	const &AMateria::getType() const{
+	return (this->_type);
+}
+
+void	AMateria::use(ICharacter& target){
 	;
 }
