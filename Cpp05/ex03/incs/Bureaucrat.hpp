d7ecb5a1f095693f3539497c1545dcf4e6448f9e
tang1304 <tanguy.gellon@gmail.com>
2023-11-23 15:43:28 +0100

    working on cpp05 ex03

---
+++ Cpp05/ex03/incs/Bureaucrat.hpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 13:57:46 by tgellon           #+#    #+#             */
+/*   Updated: 2023/11/23 13:28:54 by tgellon          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define PINK "\033[35m"
+# define DARK_GREY "\033[30m"
+# define WHITE "\033[0m"
+
+# include <iostream>
+
+class AForm;
+
+class Bureaucrat{
+private:
+
+	const std::string	_name;
+	int	_grade;
+	Bureaucrat();
+
+public:
+
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat(const Bureaucrat &old);
+	~Bureaucrat();
+	Bureaucrat &operator=(const Bureaucrat &old);
+
+	std::string	getName() const;
+	int			getGrade() const;
+	void		increment();
+	void		decrement();
+	void		signForm(AForm &form);
+
+	class GradeTooHigh: public std::exception{
+		public:
+		const char *what() const throw();
+	};
+	class GradeTooLow: public std::exception{
+		public:
+		const char *what() const throw();
+	};
+};
+
+std::ostream	&operator<<(std::ostream &out, const Bureaucrat &rhs);
+
+#endif
---
+++ Cpp05/ex03/srcs/RobotomyRequestForm.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.cpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 10:40:49 by tgellon           #+#    #+#             */
+/*   Updated: 2023/11/23 13:38:09 by tgellon          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incs/RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm(): AForm("RobotomyRequest", 72, 45), 
+_target("none"){}
+
+RobotomyRequestForm::RobotomyRequestForm(std::string target):
+	AForm("RobotomyRequest", 72, 45), _target(target){}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &old): AForm(old){
+	*this = old;
+}
+
+RobotomyRequestForm::~RobotomyRequestForm(){
+}
+
+RobotomyRequestForm &RobotomyRequestForm::operator=(const RobotomyRequestForm &old){
+	if (this != &old){
+		this->_target = old._target;
+		this->setSigned(old.getSigned());
+	}
+	return (*this);
+}
+
+void	RobotomyRequestForm::execute(Bureaucrat const &executor) const{
+	if (!this->getSigned())
+		throw FormNotSigned();
+	if (executor.getGrade() > this->getExecGrade())
+		throw GradeTooLow();
+	std::cout << BLUE << "Makes some drilling noises" << WHITE << std::endl;
+	srand(std::time(NULL));
+	if (rand() % 2 == 0)
+		std::cout << GREEN << this->_target << " has been succesfully robotomized !" << WHITE << std::endl;
+	else
+		std::cout << YELLOW << this->_target << " has not been robotomized..." << WHITE << std::endl;
+}
