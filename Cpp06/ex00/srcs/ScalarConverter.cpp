99c2e9d0403abd67beb516132abf6defdcaff5f7
tang1304 <tanguy.gellon@gmail.com>
2023-11-24 15:50:16 +0100

    start cpp06 ex00

---
+++ Cpp06/ex00/srcs/ScalarConverter.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 09:59:04 by tgellon           #+#    #+#             */
+/*   Updated: 2023/11/24 15:39:37 by tgellon          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter(){
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &old){
+	
+}
+
+ScalarConverter::~ScalarConverter(){
+}
+
+ScalarConverter	&ScalarConverter::operator=(const ScalarConverter &old){
+	return (*this);
+}
+
+static void	convert(const std::string literal){
+	std::
+}
