8cddc66a8dddcc30ba3d4aacd03d30ae31876485
Tanguy Gellon <tgellon@z4r8p5.42lyon.fr>
2023-12-07 16:58:40 +0100

    cpp08 ex01 working, needs checks

--- Cpp08/ex01/srcs/Span.cpp
+++ Cpp08/ex01/srcs/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 09:46:52 by tgellon           #+#    #+#             */
-/*   Updated: 2023/12/07 11:46:20 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/12/07 16:55:43 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,22 +38,45 @@ void	Span::addNumber(int n){
 }
 
 unsigned int	Span::shortestSpan(){
+	unsigned int	min = INT_MAX;
+	int	prev;
+	std::vector<int>::iterator	it;
+	std::vector<int> cpy = this->_vContainer;
+
 	if (this->_N <= 1 || this->_vContainer.size() <= 1)
 		throw (NotEnoughElements());
-	std::vector<int>::iterator it;
-	
+	std::sort(cpy.begin(), cpy.end());
+	it = cpy.begin();
+	while (it != cpy.end() - 1){
+		prev = *it;
+		it += 1;
+		if (abs(*it - prev) < min)
+			min = abs(*it - prev);
+	}
+	return (min);
 }
 
 unsigned int	Span::longestSpan(){
+	int minElem;
+	int maxElem;
+
 	if (this->_N <= 1 || this->_vContainer.size() <= 1)
 		throw (NotEnoughElements());
-	;
+	minElem = *std::min_element(this->_vContainer.begin(), this->_vContainer.end());
+	maxElem = *std::max_element(this->_vContainer.begin(), this->_vContainer.end());
+	return (abs(maxElem - minElem));
+}
+
+void	Span::fillSpan(unsigned int nbrElements, int val){
+	if ((this->_vContainer.size() + nbrElements) > this->_N)
+
+	this->_vContainer.insert(this->_vContainer.end(), nbrElements, val);
 }
 
 const char	*Span::TooMuchElements::what() const throw(){
-	return ("There is already the maximum number of elements");
+	return ("Error, there is already the maximum number of elements");
 }
 
 const char	*Span::NotEnoughElements::what() const throw(){
-	return ("There is not enough elements to find a span");
+	return ("Error, there is not enough elements to find a span");
 }
