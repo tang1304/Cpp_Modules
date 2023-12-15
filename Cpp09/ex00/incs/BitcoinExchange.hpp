d851b48ee62364999fb5f29f6ee4d1bc1bcb9884
Tanguy Gellon <tgellon@z4r8p5.42lyon.fr>
2023-12-15 16:57:44 +0100

    starting cpp09

---
+++ Cpp09/ex00/incs/BitcoinExchange.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 10:28:54 by tgellon           #+#    #+#             */
+/*   Updated: 2023/12/15 15:38:20 by tgellon          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <map>
+
+class BitcoinExchange{
+private:
+
+	std::map<std::string , float>	_btcPrices;
+
+public:
+	BitcoinExchange();
+	~BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange &other);
+	BitcoinExchange &operator=(const BitcoinExchange &other);
+
+	
+};
+
+#endif
