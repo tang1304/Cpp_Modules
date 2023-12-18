00302409b7aaaa1b04701951ca0c99481debca16
Tanguy Gellon <tgellon@z4r8p5.42lyon.fr>
2023-12-18 16:24:18 +0100

    cpp09 ex00: parsing on date done, needs tests

--- Cpp09/ex00/incs/BitcoinExchange.hpp
+++ Cpp09/ex00/incs/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 10:28:54 by tgellon           #+#    #+#             */
-/*   Updated: 2023/12/15 15:38:20 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/12/18 15:57:02 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define BITCOINEXCHANGE_HPP
 
 # include <iostream>
+# include <fstream>
+# include <sstream>
 # include <map>
 
 class BitcoinExchange{
@@ -27,7 +29,11 @@ public:
 	BitcoinExchange(const BitcoinExchange &other);
 	BitcoinExchange &operator=(const BitcoinExchange &other);
 
-	
+	void		readUserFile(std::string fileName);
+	std::string	checkDate(std::string content);
+	int			yearCheck(std::string year);
+	int			monthCheck(std::string month);
+	int			dayCheck(std::string day, int month, int year);
 };
 
 #endif
